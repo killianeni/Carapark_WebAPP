23ccc801cb34bfe3298e30a4b573162fa7c6a767
Patrick Morel <patrick.morel2017@campus-eni.fr>
2020-04-02 15:41:14 +0200

    configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,49 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    jest: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "plugin:vue/strongly-recommended",
+    "eslint:recommended"
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
+    "vue/max-attributes-per-line": ["warn", {
+      "singleline": 4,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
       }
-    }
-  ]
-}
+    }],
+    "vue/html-self-closing": ["warn", {
+      "html": {
+        "void": "any",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
+    "no-console": ["off"],
+    "quotes": ["error", "single", { "allowTemplateLiterals": true }],
+    "guard-for-in": "error",
+    "quote-props": ["error", "as-needed"],
+    "arrow-parens": ["warn", "as-needed"],
+    "no-unneeded-ternary": ["error", { "defaultAssignment": true }],
+    "prefer-const": "warn",
+    "spaced-comment": "warn",
+    "no-param-reassign": ["warn", { "props": false }],
+
+    "getter-return": "off",
+    "semi": [1, "always"],
+    "consistent-return": "error"
+  },
+  parserOptions: {
+    "ecmaFeatures": {
+      "legacyDecorators": true
+    },
+    parser: "babel-eslint"
+  }
+};
