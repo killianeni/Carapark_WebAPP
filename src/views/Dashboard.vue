1dd85059be1c0ff011c0edfc9cc66e6c53f886b3
Patrick Morel <patrick.morel2017@campus-eni.fr>
2020-06-19 11:47:36 +0200

    feature/Update session storage |

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,21 +5,7 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col">
-              1 of 2
-            </div>
-            <div class="col">
-              2 of 2
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              1 of 3
-            </div>
-            <div class="col">
-              2 of 3
-            </div>
-            <div class="col">
-              3 of 3
+              Calendrier
             </div>
           </div>
         </div>
